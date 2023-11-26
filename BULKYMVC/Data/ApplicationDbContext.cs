594d23b6f8186fec4b94f609c024057b4cb52bd1
Mohamed Hamouda Abbas <mk7300143@gmail.com>
2023-11-26 23:11:22 +0200

    Add project files.

---
+++ BULKYMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BULKYMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BULKYMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { Id = 1, Name = "Actions", DisPlayOrder = 1 },
+                new Category { Id = 2, Name = "Actions", DisPlayOrder = 2 },
+                new Category { Id = 3, Name = "Actions", DisPlayOrder = 3 }
+              );
+        }
+    }
+}
